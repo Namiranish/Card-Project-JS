730a746584f06b54d9fcabb8b339a2d1613b51da
Namirah anis <namiranish@gmail.com>
2025-01-16 22:06:18 +0530

    updated few things

--- script.js
+++ script.js
@@ -1,6 +1,7 @@
 const details = document.getElementById('details');
+const themeToggleBtn = document.getElementById('theme-toggle-btn');
 
-function collectData(){
+function collectData() {
     let firstName = prompt('Enter First Name');
     let lastName = prompt('Enter Last Name');
     let country = prompt('Enter Country');
@@ -17,30 +18,48 @@ function collectData(){
         state,
         city,
         village
-    }
-    
-    localStorage.setItem('userInfo', JSON.stringify(Userdata)); //converted obj to string
-    displayData()
+    };
+
+    localStorage.setItem('userInfo', JSON.stringify(Userdata));
+    displayData(); 
 }
 
-function displayData(){
+function displayData() {
     let savedData = localStorage.getItem('userInfo');
-    let Userdata = JSON.parse(savedData);
-    if(Userdata){
-        details.innerHTML = 
-        `<ul>
-        <li>First Name:<span>${Userdata.firstName}</span></li>
-        <li>Last Name:<span>${Userdata.lastName}</span></li>
-        <li>Country:<span>${Userdata.country}</span></li>
-        <li>Phone Number:<span>${Userdata.phoneNumber}</span></li>
-        <li>State:<span>${Userdata.state}</span></li>
-        <li>City:<span>${Userdata.city}</span></li>
-        <li>Village:<span>${Userdata.village}</span></li>
-    </ul>`
+    if (savedData) {
+        let Userdata = JSON.parse(savedData);
+        details.innerHTML = `
+            <ul>
+                <li>First Name: <span>${Userdata.firstName || "N/A"}</span></li>
+                <li>Last Name: <span>${Userdata.lastName || "N/A"}</span></li>
+                <li>Country: <span>${Userdata.country || "N/A"}</span></li>
+                <li>Phone Number: <span>${Userdata.phoneNumber || "N/A"}</span></li>
+                <li>State: <span>${Userdata.state || "N/A"}</span></li>
+                <li>City: <span>${Userdata.city || "N/A"}</span></li>
+                <li>Village: <span>${Userdata.village || "N/A"}</span></li>
+            </ul>`;
+    } else {
+        details.innerHTML = "<p>No user data found. Please enter information.</p>";
     }
 }
 
-window.onload = ()=>{
-    collectData()
-    displayData()
+function toggleTheme() {
+    const body = document.body;
+    body.classList.toggle('dark-theme');
+
+    const isDarkMode = body.classList.contains('dark-theme');
+    localStorage.setItem('theme', isDarkMode ? 'dark' : 'light');
 }
+
+
+window.onload = () => {
+    collectData();
+    displayData();
+
+ 
+    const savedTheme = localStorage.getItem('theme');
+    if (savedTheme === 'dark') {
+        document.body.classList.add('dark-theme');
+    }
+    themeToggleBtn.addEventListener('click', toggleTheme);
+};
